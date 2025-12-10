d04f9a7c4343ce6c66c375ef8898e6baa34ce8d9
saske-pudjiha <kirigole2005@gmail.com>
2025-12-10 23:07:05 +0700

    final

--- src/api/index.ts
+++ src/api/index.ts
@@ -9,9 +9,13 @@ type RESPONSE_DATA = {
 };
 
 const getJson = async <T>(endpoint: ENDPOINTS): Promise<T> => {
-    const path = `http://localhost:3001/api/${endpoint}`;
+    const path =
+    process.env.NODE_ENV === 'development'
+    ? `http://localhost:3001/api/${endpoint}`
+    : `https://raw.githubusercontent.com/saske-pudjiha/gh-pages-demo/gh-pages/static/db/${endpoint}.json`;
+
     const response = await fetch(path);
-    
+
     return await response.json();
 };
 
